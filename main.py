6ee991fdc61dfcae5f3fc1085838d1e55e2beddb
EmanuelVictorSilva <emanuelvictorsilva768@gmail.com>
2023-05-26 09:07:18 -0300

    Exercício Concluído

---
+++ main.py
@@ -0,0 +1,15 @@
+a = int(input("Digite aqui:"))
+if a < 0:
+         print("Impossível!")
+if  (a < 18 and a > 0 or a == 0):
+              print("Não precisa se alistar.")
+if  (a > 18 and a < 65) :
+        print("Não esqueça de votar na próxima eleição.")
+if a > 65:
+        print("Vá descansar.")
+if a == 18:
+        print("Eita!")
+if a == 65:
+        print("Eita!")
+    
+
